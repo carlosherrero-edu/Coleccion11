93accf37dd9dfaf7c5969424743a74398f888803
cherre1 <carloscherrero@yahoo.es>
2022-04-25 18:09:30 +0200

    ESqueleto de Ejercicio 9

--- src/ejercicio9/Ejercicio9.java
+++ src/ejercicio9/Ejercicio9.java
@@ -1,12 +1,17 @@
 package ejercicio9;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Ejercicio9 {
 	
 	static Scanner teclado= new Scanner(System.in);
 	
 	static final String SEPARADOR="[\\p{Space}\\p{Punct}]\\p{Space}*";
+	
+	static Map<String, Integer> diccionario= new TreeMap<>();
 
 	public static void main(String[] args) {
 		
@@ -15,10 +20,32 @@ public class Ejercicio9 {
 		
 		String[] palabras= frase.toLowerCase().split(SEPARADOR);
 		
-		
+		//recorre la lista de palabras que he obtenido
 		for (String termino: palabras) {
-			System.out.format(" %s, ", termino);
+			
+			//comprobar si la palabra está ya en el diccionario
+			if (diccionario.containsKey(termino)) {
+				//obtengo el número de veces que se había repetido
+				int repeticiones= diccionario.get(termino);
+				//lo vuelvo a guardar con ese número incrementado en 1
+				diccionario.replace(termino, repeticiones+1);
+				
+			} else {
+				//falso: el término no está en el diccionario
+				diccionario.put(termino, 1);
+			}
+			
 		}
+		
+		//mostramos el diccionario
+		Set<String> indices=diccionario.keySet();
+		
+		
+		for (String termino: indices) {
+			System.out.format("\n %s : %2d", termino, diccionario.get(termino));
+			
+		}
+		
 		
 	}
 
