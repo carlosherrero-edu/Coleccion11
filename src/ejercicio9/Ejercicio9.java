f48405c7c1995b6d97b0b1a24fecda36ef27fb84
cherre1 <carloscherrero@yahoo.es>
2022-04-25 16:53:49 +0200

    Plantilla para plantear el ejercicio 9

---
+++ src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,25 @@
+package ejercicio9;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+	
+	static Scanner teclado= new Scanner(System.in);
+	
+	static final String SEPARADOR="[\\p{Space}\\p{Punct}]\\p{Space}*";
+
+	public static void main(String[] args) {
+		
+		System.out.println("Escribe tu frase");
+		String frase= teclado.nextLine();
+		
+		String[] palabras= frase.split(SEPARADOR);
+		
+		
+		for (String termino: palabras) {
+			System.out.format(" %s, ", termino);
+		}
+		
+	}
+
+}
