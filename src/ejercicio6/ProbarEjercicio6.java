21c9b2f4c2ca645befb49b2c427283cc6bee1df3
cherre1 <carloscherrero@yahoo.es>
2022-05-03 12:50:41 +0200

    Clase de prueba para el ejercicio 6

---
+++ src/ejercicio6/ProbarEjercicio6.java
@@ -0,0 +1,58 @@
+package ejercicio6;
+import java.util.TreeSet;
+
+public class ProbarEjercicio6 {
+
+	public static void main (String[] args) {
+		// Creo varias instancias de alumno
+		
+		Alumno alu1=new Alumno (100001, "Luis","Berlanga",4);
+		Alumno alu2=new Alumno (200002, "Pilar","Miro",4);
+		Alumno alu3=new Alumno (300003, "Iztiar","Boallin",4);
+		Alumno alu4=new Alumno (400004, "Luis","Bu�uel",4);
+		Alumno alu5=new Alumno (500005, "Isabel","Coixet",4);
+		Alumno alu6=new Alumno (600006, "Mario","Camus",4);
+		
+		// a continuación les asigno calificaciones
+		// 3 aprueban y 3 suspenden
+		alu1.asignarCalificaciones( new double[]{6.5, 3.5,7,4.25} ); //media:5.3125
+		alu2.asignarCalificaciones( new double[]{6.25, 3.5,7,4.25} ); //media: 5.25
+		alu3.asignarCalificaciones( new double[]{6.5, 3.5,7.5,4.25} ); //media: 5.4375
+		alu4.asignarCalificaciones( new double[]{6.5, 3.5,5,3.25} ); // media: 4.5625
+		alu5.asignarCalificaciones( new double[]{6.25, 3.5,5,3.25} ); // media: 4.5
+		alu6.asignarCalificaciones( new double[]{6.5, 3.5,5.5,3.25} ); // media: 4.6875
+		
+		//creo el array de alumnos
+		Alumno[]  arrAlu= { alu1, alu2, alu3, alu4, alu5, alu6};
+		
+
+		//ahora construimos una instancia de TreeSet
+		ListaOrdenadaAlumnos loAlu= new ListaOrdenadaAlumnos(arrAlu);
+		
+		//llamamos a sus métodos para obtener los alumnos aprobados y suspensos
+		TreeSet<Alumno> arbolAprobados= loAlu.obtenerAlumnosAprobados();
+		TreeSet<Alumno> arbolSuspensos= loAlu.obtenerAlumnosSuspensos();
+		
+		//recorremos las listas para comprobarlo
+		
+		System.out.println("\nLista ordenada de alumnos aprobados");
+				for (  Alumno alu:arbolAprobados){
+					System.out.println(alu);
+				}
+				
+		System.out.println("\n Lista ordenada de alumnos suspensos");
+				for (  Alumno alu:arbolSuspensos){
+					System.out.println(alu);
+		}
+		
+		
+		
+		
+		
+		
+		}//fin método main
+		
+		
+	
+
+}
